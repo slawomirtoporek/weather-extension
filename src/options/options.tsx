72bd29e35e65088d2e362e69151a775e7bcbe06f
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-09-07 22:14:35 +0200

    Add option to select home city name

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,12 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from '@material-ui/core'
+import 'fontsource-roboto'
 import './options.css'
+import {
+  getStoredOptions,
+  LocalStorageOptions,
+  setStoredOptions,
+} from '../utils/storage'
+
+type FormState = 'ready' | 'saving...'
 
 const App: React.FC<{}> = () => {
+  const [options, setOptions] = useState<LocalStorageOptions | null>(null)
+  const [formState, serFormState] = useState<FormState>('ready')
+
+  useEffect(() => {
+    getStoredOptions().then((options) => setOptions(options))
+  }, [])
+
+  const handleHomeCityChange = (homeCity: string) => {
+    setOptions({
+      ...options,
+      homeCity,
+    })
+  }
+
+  const handleSaveButtonClick = () => {
+    serFormState('saving...')
+    setStoredOptions(options).then(() => {
+      setTimeout(() => {
+        serFormState('ready')
+      }, 1000)
+    })
+  }
+
+  if (!options) {
+    return null
+  }
+
+  const isFieldsDisabled = formState === 'saving...'
+
   return (
-    <div>
-      <img src="icon.png" />
-    </div>
+    <Box sx={{ mx: '10%', my: '2%' }}>
+      <Card>
+        <CardContent>
+          <Grid container direction="column" spacing={4}>
+            <Grid item>
+              <Typography variant="h4">Weather Extension Options</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">Home city name</Typography>
+              <TextField
+                fullWidth
+                placeholder="Enter a home city name"
+                value={options.homeCity}
+                onChange={(event) => handleHomeCityChange(event.target.value)}
+                disabled={isFieldsDisabled}
+              />
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSaveButtonClick}
+                disabled={isFieldsDisabled}
+                className="saveBtn"
+              >
+                {formState === 'ready' ? 'Save' : 'Saving...'}
+              </Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Box>
   )
 }
 
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -89,6 +89,10 @@ const App: React.FC<{}> = () => {
           </Grid>
         </Grid>
       </Box>
+      {
+        options.homeCity != '' && (
+        <WeatherCard city={options.homeCity} tempScale={options.tempScale} />
+      )}
       {cities.map((city, index) => (
         <WeatherCard
           city={city}
